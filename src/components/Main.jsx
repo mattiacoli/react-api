a5101c5c9a766b263302e3efd39568c749cb6341
mattiacoli <mattia.coli@aol.com>
2025-03-28 17:52:08 +0100

    edit Main style

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,13 +5,13 @@ export default function Main({ base_url, posts, deletePost }) {
         <section>
           <div className="container">
             {/* table structure */}
-            <table className="table table-striped table-inverse table-responsive">
+            <table className="table table-striped table-inverse table-responsive mt-4">
               <thead className="thead-inverse">
                 <tr>
                   <th>ID</th>
-                  <th>Title</th>
+                  <th className="text-center">Title</th>
                   <th>Thumb</th>
-                  <th>Actions</th>
+                  <th className="text-center">Actions</th>
                 </tr>
               </thead>
 
@@ -22,7 +22,7 @@ export default function Main({ base_url, posts, deletePost }) {
                     <td scope="row">{post.id}</td>
                     <td>{post.title}</td>
                     <td><img src={base_url + post.image} alt={post.title} /></td>
-                    <td>
+                    <td className="text-center">
                       <button type="button" className="btn btn-danger" onClick={() => deletePost(post.slug)}><i className="fa fa-trash" aria-hidden="true"></i></button>
                     </td>
                   </tr>
